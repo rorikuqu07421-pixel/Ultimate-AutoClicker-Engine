987ce45df87a7f283fa00ac07657b1d1f9214fc2
n.cole_ <rorikuqu07421@gmail.com>
2026-04-17 18:00:27 -0400

    Add implement custom exceptions for handling errors

--- exceptions.py
+++ exceptions.py
@@ -1,26 +1,37 @@
 class AutoClickerError(Exception):
-    """Base class for all exceptions related to the AutoClicker engine."""
+    """Base class for exceptions in the AutoClicker module."""
     pass
 
-class ClickRateExceededError(AutoClickerError):
-    """Raised when the click rate exceeds the maximum allowed rate."""
-    def __init__(self, rate):
-        super().__init__(f'Click rate {rate} exceeds maximum allowed rate.')
-        self.rate = rate
-
 class ConfigurationError(AutoClickerError):
-    """Raised when there is an error in the configuration settings."""
+    """Raised for errors in configuration settings."""
     def __init__(self, message):
-        super().__init__(message)
+        self.message = message
+        super().__init__(self.message)
 
-class InvalidClickTargetError(AutoClickerError):
-    """Raised when the specified click target is invalid."""
-    def __init__(self, target):
-        super().__init__(f'Invalid click target specified: {target}')
-        self.target = target
+class ClickRateError(AutoClickerError):
+    """Raised when click rate is invalid."""
+    def __init__(self, rate):
+        self.rate = rate
+        self.message = f"Invalid click rate: {self.rate}."
+        super().__init__(self.message)
 
-class ClickFrequencyError(AutoClickerError):
-    """Raised when the click frequency is set incorrectly."""
-    def __init__(self, frequency):
-        super().__init__(f'Click frequency {frequency} is not valid.')
-        self.frequency = frequency
+class TimeoutError(AutoClickerError):
+    """Raised when an operation times out."""
+    def __init__(self, timeout):
+        self.timeout = timeout
+        self.message = f"Operation timed out after {self.timeout} seconds."
+        super().__init__(self.message)
+
+class NotConfiguredError(AutoClickerError):
+    """Raised when the autoclicker is not configured properly."""
+    def __init__(self):
+        self.message = "The AutoClicker has not been configured."
+        super().__init__(self.message)
+
+__all__ = [
+    'AutoClickerError',
+    'ConfigurationError',
+    'ClickRateError',
+    'TimeoutError',
+    'NotConfiguredError'
+]
