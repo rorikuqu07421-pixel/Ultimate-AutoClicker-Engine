73cd8d111669a39416ad36b9a9333e6aa37a23d2
n.cole_ <rorikuqu07421@gmail.com>
2026-04-17 22:01:04 -0400

    feat:implement logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,43 +1,19 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
-class CustomLogger:
-    def __init__(self, name):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        self._create_console_handler()
-        self._create_file_handler()
+def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
 
-    def _create_console_handler(self):
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
+    if not logger.hasHandlers():
+        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        console_handler.setFormatter(formatter)
-        self.logger.addHandler(console_handler)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    
+    return logger
 
-    def _create_file_handler(self):
-        file_handler = logging.FileHandler('app.log')
-        file_handler.setLevel(logging.ERROR)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter)
-        self.logger.addHandler(file_handler)
-
-    def debug(self, message):
-        self.logger.debug(message)
-
-    def info(self, message):
-        self.logger.info(message)
-
-    def warning(self, message):
-        self.logger.warning(message)
-
-    def error(self, message):
-        self.logger.error(message)
-
-    def critical(self, message):
-        self.logger.critical(message)
-
-# Example usage:
 if __name__ == '__main__':
-    logger = CustomLogger(__name__)
-    logger.info('This is an info message')
-    logger.error('This is an error message')
+    my_logger = setup_logger(log_file='app.log')
+    my_logger.info('Logger is set up and ready to use!')
