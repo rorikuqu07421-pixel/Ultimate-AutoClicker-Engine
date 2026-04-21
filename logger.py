5094f52165c5d71691407c7f80eb60d91777fdb2
n.cole_ <rorikuqu07421@gmail.com>
2026-04-20 22:01:38 -0400

    optimize logger for better performance

--- logger.py
+++ logger.py
@@ -1,26 +1,31 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-class CustomLogger:
-    def __init__(self, name, log_file, max_bytes=5 * 1024 * 1024, backup_count=3):
+class Logger:
+    def __init__(self, name='UltimateAutoClicker', level=logging.INFO):
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        self.logger.setLevel(level)
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
-    def debug(self, message):
-        self.logger.debug(message)
+    def debug(self, msg):
+        self.logger.debug(msg)
 
-    def info(self, message):
-        self.logger.info(message)
+    def info(self, msg):
+        self.logger.info(msg)
 
-    def warning(self, message):
-        self.logger.warning(message)
+    def warning(self, msg):
+        self.logger.warning(msg)
 
-    def error(self, message):
-        self.logger.error(message)
+    def error(self, msg):
+        self.logger.error(msg)
 
-    def critical(self, message):
-        self.logger.critical(message)
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+# Usage example:
+if __name__ == '__main__':
+    log = Logger()
+    log.info('AutoClicker started successfully!')
