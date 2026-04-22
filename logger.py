26ca8246d38a879d38a3e4726390236829fad25b
n.cole_ <rorikuqu07421@gmail.com>
2026-04-22 18:00:17 -0400

    fix:add logging functionality and docstrings

--- logger.py
+++ logger.py
@@ -1,27 +1,47 @@
 import logging
 
-# Configuring the logger
-logger = logging.getLogger('AutoClickerLogger')
+logger = logging.getLogger("UltimateAutoClicker")
 logger.setLevel(logging.DEBUG)
 
-handler = logging.FileHandler('autoclicker.log')
-handler.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
+file_handler = logging.FileHandler("app.log", mode='a')
+file_handler.setLevel(logging.DEBUG)
 
-logger.addHandler(handler)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
 
-def log_click_event(click_data):
-    if not isinstance(click_data, dict):
-        logger.error('Invalid click data format')
-        return
-    logger.info('Click event recorded: %s', click_data)
-
-
-def log_error(error_message):
-    logger.error('Error occurred: %s', error_message)
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 
-def log_info(info_message):
-    logger.info('Info: %s', info_message)
+def log_info(message: str) -> None:
+    """
+    Logs an informational message.
+    
+    Args:
+        message (str): The message to log.
+    """
+    logger.info(message)
+
+
+def log_error(message: str) -> None:
+    """
+    Logs an error message.
+    
+    Args:
+        message (str): The message to log.
+    """
+    logger.error(message)
+
+
+def log_debug(message: str) -> None:
+    """
+    Logs a debug message.
+    
+    Args:
+        message (str): The message to log.
+    """
+    logger.debug(message)
