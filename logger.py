af24bc640e9167b27768f270e412f933ecb6529e
n.cole_ <rorikuqu07421@gmail.com>
2026-04-24 02:16:35 -0400

    feat:implement logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,13 +1,31 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
-    logger = logging.getLogger('UltimateAutoClicker')
-    logger.setLevel(logging.INFO)
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+class CustomLogger:
+    def __init__(self, name, log_file='app.log', max_bytes=10*1024*1024, backup_count=5):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
 
-logger = setup_logger()
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+# Example usage
+if __name__ == '__main__':
+    logger = CustomLogger(__name__)
+    logger.info('Logger initialized successfully.')
