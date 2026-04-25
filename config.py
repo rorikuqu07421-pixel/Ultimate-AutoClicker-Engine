e97c4dab182dd3937e237dc1382a8cf33f309207
n.cole_ <rorikuqu07421@gmail.com>
2026-04-24 22:31:41 -0400

    feat:implement configuration loader with defaults

--- config.py
+++ config.py
@@ -1,28 +1,34 @@
 import json
+import os
 
-DEFAULT_CONFIG = {
+DEFAULTS = {
     'click_interval': 0.1,
-    'max_clicks': 100,
-    'click_type': 'left',
-    'click_position': [0, 0],
+    'max_clicks': 1000,
+    'duration': 10,
     'enabled': True
 }
 
 class ConfigLoader:
-    def __init__(self, filename='config.json'):
-        self.filename = filename
-        self.config = self.load_config()
+    def __init__(self, config_file='config.json'):
+        self.config_file = config_file
+        self.config = DEFAULTS.copy()
+        self.load_config()
 
     def load_config(self):
-        try:
-            with open(self.filename, 'r') as config_file:
-                user_config = json.load(config_file)
-                return {**DEFAULT_CONFIG, **user_config}
-        except FileNotFoundError:
-            return DEFAULT_CONFIG
-        except json.JSONDecodeError:
-            print('Error loading config, using defaults.')
-            return DEFAULT_CONFIG
+        if os.path.isfile(self.config_file):
+            with open(self.config_file, 'r') as file:
+                user_config = json.load(file)
+                self.config.update(user_config)
+
+    def get(self, key, default=None):
+        return self.config.get(key, default)
+
+    def set(self, key, value):
+        self.config[key] = value
+
+    def save(self):
+        with open(self.config_file, 'w') as file:
+            json.dump(self.config, file, indent=4)
 
 if __name__ == '__main__':
     loader = ConfigLoader()
