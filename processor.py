c42197383eb01f0790bacfafc76426803e5b8d22
Derek Kim <rorikuqu07421@gmail.com>
2026-04-27 22:01:50 -0400

    Add cleanup and reorganize clicker code

--- processor.py
+++ processor.py
@@ -1,38 +1,31 @@
 import time
 import threading
-from queue import Queue
 
-class ClickProcessor:
-    def __init__(self, click_interval=0.1):
-        self.click_interval = click_interval
-        self.is_running = False
-        self.queue = Queue()
-
-    def start_auto_clicker(self):
-        self.is_running = True
-        threading.Thread(target=self._process_queue, daemon=True).start()
-
-    def stop_auto_clicker(self):
+class Clicker:
+    def __init__(self, interval):
+        self.interval = interval
         self.is_running = False
 
-    def _process_queue(self):
+    def start(self):
+        if not self.is_running:
+            self.is_running = True
+            self.click_thread = threading.Thread(target=self._click_loop)
+            self.click_thread.start()
+
+    def _click_loop(self):
         while self.is_running:
-            if not self.queue.empty():
-                self._perform_click(self.queue.get_nowait())
-            time.sleep(self.click_interval)
+            self._perform_click()
+            time.sleep(self.interval)
 
-    def queue_click(self, click_coordinates):
-        self.queue.put(click_coordinates)
+    def _perform_click(self):
+        print('Click!')  # Simulate a mouse click
 
-    def _perform_click(self, coords):
-        # Simulated click action at specified coordinates
-        print(f"Clicking at {coords}")
+    def stop(self):
+        self.is_running = False
+        self.click_thread.join()
 
 if __name__ == '__main__':
-    processor = ClickProcessor()
-    processor.start_auto_clicker()
-    processor.queue_click((100, 200))
-    time.sleep(1)
-    processor.queue_click((150, 250))
-    time.sleep(1)
-    processor.stop_auto_clicker()
+    clicker = Clicker(1)
+    clicker.start()
+    time.sleep(5)
+    clicker.stop()
