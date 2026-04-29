85072b6013a4e11df4f22685428f8d7096bb45ee
Derek Kim <rorikuqu07421@gmail.com>
2026-04-29 18:16:46 -0400

    optimize click performance with threading

--- helpers.py
+++ helpers.py
@@ -1,45 +1,30 @@
 import time
-import random
+import threading
 
+class ClickHelper:
+    def __init__(self, click_interval=0.1):
+        self.click_interval = click_interval
+        self._stop_event = threading.Event()
 
-def sleep_random(min_seconds, max_seconds):
-    delay = random.uniform(min_seconds, max_seconds)
-    time.sleep(delay)
-    return delay
+    def start_clicking(self):
+        self._stop_event.clear()
+        threading.Thread(target=self._click_loop).start()
 
+    def stop_clicking(self):
+        self._stop_event.set()
 
-def get_current_time():
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    def _click_loop(self):
+        while not self._stop_event.is_set():
+            self.perform_click()
+            time.sleep(self.click_interval)
 
+    @staticmethod
+    def perform_click():
+        # This is where you would integrate clicking functionality
+        print('Clicked!')
 
-def click_position(x, y):
-    import pyautogui
-    pyautogui.click(x, y)
-
-
-def is_hotkey_pressed(hotkey):
-    from pynput import keyboard
-    pressed = set()
-
-    def on_press(key):
-        try:
-            pressed.add(key.char)
-        except AttributeError:
-            pressed.add(key)
-
-    def on_release(key):
-        pressed.discard(key)
-        if all(k in pressed for k in hotkey):
-            return False
-        if key == keyboard.Key.esc:
-            return False
-
-    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-        listener.join()
-    return True
-
-
-def random_click(min_x, max_x, min_y, max_y):
-    x = random.randint(min_x, max_x)
-    y = random.randint(min_y, max_y)
-    click_position(x, y)
+if __name__ == '__main__':
+    click_helper = ClickHelper(click_interval=0.2)
+    click_helper.start_clicking()
+    time.sleep(2)
+    click_helper.stop_clicking()
