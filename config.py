e953d770d994ff57c1776847a901b1078c4c1c47
Derek Kim <rorikuqu07421@gmail.com>
2026-04-29 22:46:31 -0400

    fix:reorganize and clean up config file

--- config.py
+++ config.py
@@ -1,24 +1,22 @@
 import os
-import json
 
 class Config:
-    def __init__(self, config_file='settings.json'):
-        self.config_file = config_file
-        self.settings = self.load_config()
+    def __init__(self):
+        self.click_interval = 0.01  # seconds
+        self.max_clicks = 10000
+        self.output_directory = os.path.expanduser('~') + '/autoclicker'
+        self.click_sound = True
+        self.mouse_button = 'left'
+        self.hotkey = 'ctrl+c'
 
-    def load_config(self):
-        if not os.path.exists(self.config_file):
-            raise FileNotFoundError(f'Config file {self.config_file} not found.')
-        with open(self.config_file, 'r') as file:
-            return json.load(file)
+    def update_settings(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+            else:
+                raise AttributeError(f'Invalid config key: {key}')
 
-    def get(self, key, default=None):
-        return self.settings.get(key, default)
-
-    def save(self):
-        with open(self.config_file, 'w') as file:
-            json.dump(self.settings, file, indent=4)
-
-    def update(self, key, value):
-        self.settings[key] = value
-        self.save()
+    def display_settings(self):
+        settings = {k: v for k, v in self.__dict__.items()}
+        for key, value in settings.items():
+            print(f'{key}: {value}')
